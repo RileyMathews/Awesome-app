5efafd257740342e98ce0c8c40d6263119277e54
Riley Mathews <rileymathews80@gmail.com>
2018-04-26 14:39:44 -0500

    split more components out into modules

---
+++ src/scripts/cardFactory.js
@@ -0,0 +1,9 @@
+// Create card component
+const cardFactory = (classList, textContent) => {
+    const theSection = document.createElement("section")
+    theSection.classList = classList
+    theSection.textContent = textContent
+    return theSection
+}
+
+module.exports = cardFactory
